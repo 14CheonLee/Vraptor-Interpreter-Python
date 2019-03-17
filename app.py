9ceb5abcb234ced2e20411d458212b50a369dc04
ByeongGil-Jung <jbkcose@gmail.com>
2019-03-17 19:50:44 +0900

    [19-03-17] :: Modifying cmd code for dummy data - Modifying cmd code for dummy data

--- app.py
+++ app.py
@@ -9,9 +9,9 @@
  Time: 오후 5:06
 """
 
-from flask import Flask, Response
+from flask import Flask, Response, request
 
-import querycontroller as qc
+import controller as qc
 import json
 
 app = Flask(__name__)
@@ -22,28 +22,37 @@ def index():
     return "Vraptor Interpreter"
 
 
-@app.route("/get_sensor_data")
+@app.route("/get_sensor_data", methods=["POST"])
 def get_sensor_data():
-    data = qc.get_sensor_data()
+    node_num = request.form["node_num"]
+
+    data = qc.get_sensor_data(node_num=node_num)
     res = json.dumps(data)
     return Response(res, status=200, mimetype="application/json")
 
 
-@app.route("/get_fan_mode")
+@app.route("/get_fan_mode", methods=["POST"])
 def get_fan_mode():
-    data = qc.get_fan_mode()
+    fan_id = request.form["fan_id"]
+
+    data = qc.get_fan_mode(fan_id=fan_id)
     res = json.dumps(data)
     return Response(res, status=200, mimetype="application/json")
 
 
-@app.route("/set_fan_mode")
+@app.route("/set_fan_mode", methods=["POST"])
 def set_fan_mode():
-    qc.set_fan_mode()
+    fan_id = request.form["fan_id"]
+    fan_mode = request.form["fan_mode"]
+
+    qc.set_fan_mode(fan_id=fan_id, fan_mode=fan_mode)
 
 
-@app.route("/rum_power_tool")
+@app.route("/rum_power_tool", methods=["POST"])
 def run_power_tool():
-    qc.run_power_tool()
+    tag = request.form["tag"]
+
+    qc.run_power_tool(tag=tag)
 
 
 if __name__ == "__main__":
---
+++ controller.py
@@ -0,0 +1,51 @@
+"""
+ Created by IntelliJ IDEA.
+ Project: Vraptor-Interpreter
+ ===========================================
+ User: ByeongGil Jung
+ Date: 2019-03-16
+ Time: 오후 5:50
+"""
+
+import data as d
+
+import os
+
+# bin_path = "/usr/bin"
+sensortool = "sensortool"
+powertool = "powertool"
+fan = "fan"
+
+
+def cmd_call(cmd: str):
+    return os.system(cmd)
+
+
+def get_sensor_data(node_num):
+    """
+    @TODO
+    > 각 명령어에 맞춰 수정할 것
+    """
+    # data = cmd_call("sensortool --get-sensor-data --nude-num={}".format(node_num))
+    data = {"node_num": node_num, "test_data": "get_sensor_data func"}
+    return data
+
+
+def get_fan_mode(fan_id: int):
+    # data = cmd_call("sensortool --get-fan-mode --fan-id={}".format(fan_id))
+    data = {"fan_id": fan_id, "test_data": "get_fan_mode func"}
+    return data
+
+
+def set_fan_mode(fan_id: int, fan_mode: int) -> int:
+    # cmd_call("sensortool --set-fan-mode={} --fan-id={}".format(fan_mode, fan_id))
+    print(fan_id, fan_mode)
+    result_state = 1
+    return result_state
+
+
+def run_power_tool(tag: str) -> int:
+    # cmd_call("powertool --tag={}".format(tag))
+    print(tag)
+    result_state = 1
+    return result_state
